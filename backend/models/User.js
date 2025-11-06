beb6113dc5053951b81221dc25d0868dc0cca3c7
Angad Yadav <angadyadav@Angads-MacBook-Pro.local>
2025-11-06 12:37:54 +0530

    feat: team management create + UI

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
-  email: { type: String, default: null },
   password: { type: String },
   wallet: {
     type: Number,
